f0d052eb883169c07c711d0f2ec4850c27edb2a2
clement1234514 <yipdi@th-brandenburg.de>
2026-04-26 14:47:51 +0200

    Update main.tf

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -1,19 +1,20 @@
 terraform {
   required_version = ">= 1.0"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
   }
 }
 
 provider "aws" {
-  region = "eu-central-1"
-}
-
-resource "aws_s3_bucket" "bottletube" {
-  bucket = "bottletube-images-${random_id.suffix.hex}"
+  region = var.region
 }
 
 resource "random_id" "suffix" {
