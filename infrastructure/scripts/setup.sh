7ee0448d8b8687ca9755f641a21d94ca27205ac0
clement1234514 <yipdi@th-brandenburg.de>
2026-04-26 14:57:14 +0200

    Update setup.sh

--- infrastructure/scripts/setup.sh
+++ infrastructure/scripts/setup.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+apt update -y
+apt install -y python3 python3-pip apache2 libapache2-mod-wsgi-py3
+
+mkdir -p /var/www/bottletube
 cd /var/www/bottletube
-pip3 install -r requirements.txt
+
+# App wird später per Git oder AMI bereitgestellt
+pip3 install bottle boto3 psycopg2-binary
+
 systemctl restart apache2
