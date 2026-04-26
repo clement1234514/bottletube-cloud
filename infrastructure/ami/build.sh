9965ca12dbcfdba91163c773c568cee7c6a03d5b
clement1234514 <yipdi@th-brandenburg.de>
2026-04-26 15:20:40 +0200

    Update build.sh

--- infrastructure/ami/build.sh
+++ infrastructure/ami/build.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 set -e
 
-apt update
+apt update -y
 apt install -y python3 python3-pip apache2 libapache2-mod-wsgi-py3 git
 
+# App-Verzeichnis
 mkdir -p /var/www/bottletube
-cd /var/www/bottletube
 
-# App wird später per Launch Template oder Git Pull aktualisiert
+# Apache Config kopieren
+cp /tmp/bottletube.conf /etc/apache2/sites-available/bottletube.conf
+a2ensite bottletube
+a2dissite 000-default
+systemctl reload apache2
+
+# Python Dependencies
 pip3 install bottle boto3 psycopg2-binary
-
-systemctl enable apache2
