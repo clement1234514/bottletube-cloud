c9074019412a466ce65c9261115a377db2c7f0b0
clement1234514 <yipdi@th-brandenburg.de>
2026-04-26 14:45:26 +0200

    Create ec2.tf

---
+++ infrastructure/terraform/ec2.tf
@@ -0,0 +1,11 @@
+resource "aws_launch_template" "bottletube" {
+  name_prefix   = "bottletube-"
+  image_id      = var.ami_id
+  instance_type = "t3.micro"
+
+  user_data = base64encode(file("${path.module}/../scripts/setup.sh"))
+
+  network_interfaces {
+    security_groups = [aws_security_group.ec2.id]
+  }
+}
